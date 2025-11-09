da935bbeaf36652ba750e2769ba3a145a0c032f2
mfris26 <ryankaliky07@gmail.com>
2025-11-09 21:39:13 +0700

    riann

---
+++ java/variabel.java
@@ -0,0 +1,22 @@
+public class variabel {
+    public static void main(String[] args) {
+        // Deklarasi variabel dengan tipe data yang sesuai
+        String nama = " rian alfarizi kaliky";
+        String tempatTanggalLahir = "kambelo, 24 nov 2003";
+        String jenisKelamin = "Laki-laki";
+        String alamatLengkap = "Kp bojong kerta Bogor Selatan";
+        String noHp = "0895338135061";
+        Double penghasilanPerbulan = 1500000.0;
+
+        System.out.println("Assalamualaikum Wr. Wb.");
+        System.out.println("Izinkan saya memperkenalkan diri:");
+        
+        System.out.println("Nama: " + nama);
+        System.out.println("Tempat, Tanggal Lahir: " + tempatTanggalLahir);
+        System.out.println("Jenis Kelamin: " + jenisKelamin);
+        System.out.println("Alamat Lengkap: " + alamatLengkap);
+        System.out.println("No. HP: " + noHp);
+        System.out.println("Penghasilan Perbulan: Rp " + penghasilanPerbulan);
+
+    }
+}
